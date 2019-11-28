845e5d3a23f90cf472faa021d98736b05bbef578
Michihito Kato <mkato@zettant.com>
2019-11-28 16:08:16 +0900

    fix : source command don't work in linux environment

--- common_mk/deploy.mk
+++ common_mk/deploy.mk
@@ -1,5 +1,5 @@
 package:
-	@$(eval AWS_ACCOUNT_ID := $(shell source ${SAMDIR}/venv/bin/activate && aws sts get-caller-identity --profile $(PROFILE) | jq -r ."Account"))
+	@$(eval AWS_ACCOUNT_ID := $(shell . ${SAMDIR}/venv/bin/activate && aws sts get-caller-identity --profile $(PROFILE) | jq -r ."Account"))
 	@$(eval STACK_NAME_LCASE := $(shell echo $(STACK_NAME) | tr A-Z a-z))
 	@$(eval S3_TEMPLATE_BUCKET := $(AWS_ACCOUNT_ID)-$(STACK_NAME_LCASE)-sam)
 	if [[ -z `. ${SAMDIR}/venv/bin/activate && ${AWSCLI} s3 ls --profile ${PROFILE} | grep ${S3_TEMPLATE_BUCKET}-${PROFILE}` ]]; then \
