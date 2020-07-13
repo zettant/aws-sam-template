a4d31eea295095d8000e124a015a000725c36279
Genyu Suzuki <genyu1979@gmail.com>
2020-07-13 20:59:41 +0900

    make deploy時にpackage.yamlが51200バイト超過した場合の対処を実装

--- common_mk/deploy.mk
+++ common_mk/deploy.mk
@@ -13,9 +13,24 @@ package:
 
 
 deploy:
+	@$(eval PACKAGE_YAML_SIZE := $(shell wc -c < packaged.yaml))
+	$(SHELL) -c "if [ ${PACKAGE_YAML_SIZE} -gt 51200 ]; then make deploys3; else make deployslocal; fi"
+
+
+deploylocal:
 	$(SHELL) -c "\
 	    . ${SAMDIR}/venv/bin/activate && \
 	    ${AWSCLI} cloudformation deploy --profile ${PROFILE} --template-file packaged.yaml --stack-name ${STACK_NAME} --capabilities CAPABILITY_IAM --parameter-overrides DeployEnv=${DEPLOY_ENV}"
+	make update-env
+
+
+deploys3:
+	@$(eval AWS_ACCOUNT_ID := $(shell . ${SAMDIR}/venv/bin/activate && aws sts get-caller-identity --profile $(PROFILE) | jq -r ."Account"))
+	@$(eval STACK_NAME_LCASE := $(shell echo $(STACK_NAME) | tr A-Z a-z))
+	@$(eval S3_TEMPLATE_BUCKET := $(AWS_ACCOUNT_ID)-$(STACK_NAME_LCASE)-sam)
+	$(SHELL) -c "\
+	    . ${SAMDIR}/venv/bin/activate && \
+	    ${AWSCLI} cloudformation deploy --profile ${PROFILE} --template-file packaged.yaml --stack-name ${STACK_NAME} --capabilities CAPABILITY_IAM --parameter-overrides DeployEnv=${DEPLOY_ENV} --s3-bucket ${S3_TEMPLATE_BUCKET}-${PROFILE}"
 	make update-env
 
 
